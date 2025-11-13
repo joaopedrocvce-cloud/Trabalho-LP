b1e626224df7db2a138961d789e7f2639702979b
Gus10FF <gutofrancisco2@outlook.com>
2025-11-13 15:19:40 -0300

    Para o meu Mozão João Pedro

--- Login/js/modules/config.js
+++ Login/js/modules/config.js
@@ -1,4 +1,4 @@
 // js/modules/config.js
 // Substitua pelos dados do seu projeto Supabase
-export const SUPABASE_URL = 'https://xfbecxmhcmkaxxvgrhmj.supabase.co'
+export const SUPABASE_URL = 'https://hjhfazmacwxtsuwpxjrj.supabase.co'
 export const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhmYmVjeG1oY21rYXh4dmdyaG1qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3NzY2NTAsImV4cCI6MjA3ODM1MjY1MH0.YnEyoum_-eyG8oGWTOa06dX5wBhzKUGNVXMj9bvoMVU'
